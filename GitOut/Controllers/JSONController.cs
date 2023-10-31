0b704ce17edf961e4847af5b33be404ae76173d0
adhik <adhik@DINESH>
2023-10-31 16:25:08 -0400

    Second commit

---
+++ GitOut/Controllers/JSONController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitOut.Controllers
+{
+    public class JSONController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetJson()
+        {
+            string output = false ? "Dinesh Adhikari" : "Dinesh Adhikari";
+
+            return Json(output);
+        }
+    }
+}
---
+++ GitOut/Controllers/Name.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitOut.Controllers
+{
+    public class Name : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult name()
+        {
+            string name = "";
+
+            string output = false ? "Dinesh Adhikari": "Dinesh Adhikari";
+
+            return Json(output);
+        }
+    }
+}
